591ffe227ee9b9b0f0b52cfa5090f450a9bc6a07
Miariniaina04 <miariniaina04n@gmail>
2026-07-20 17:09:03 +0300

    css2

--- codeigniter4-framework-b3359be/app/Views/client/dashboard.php
+++ codeigniter4-framework-b3359be/app/Views/client/dashboard.php
@@ -4,7 +4,13 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dashboard client</title>
-    <link href="<?= base_url('css/style.css') ?>" rel="stylesheet" />
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600&family=Syne:wght@700;800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url('css/bootstrap.css') ?>">
+
 </head>
 <body style="background-color: var(--bs-body-bg); min-height: 100vh;">
 
